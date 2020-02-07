e96c6891616814e4899efc191d1914153c538539
KevinHWS <kevinshu1995@gmail.com>
2020-02-08 03:59:50 +0800

    sidebar-view - done

--- js/all.js
+++ js/all.js
@@ -1,6 +1,5 @@
 var map;
 var maskData;
-var marker;
 var popup;
 var countryForm = document.getElementById('countryForm');
 var zoneForm = document.getElementById('zoneForm');
@@ -8,6 +7,8 @@ var zoneForm = document.getElementById('zoneForm');
 var sidebar = L.control.sidebar('sidebar', {
   position: 'left'
 });
+//sidebarbtn
+// var showSide = document.querySelector('.showSide');
 
 //set default location
 map = L.map('map').setView([23.5, 120.5], 8);
@@ -21,6 +22,7 @@ countryForm.addEventListener('change', getValue)
 //執行sidebar
 map.addControl(sidebar);
 
+
 //建立地圖
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '<a href="https://www.openstreetmap.org/">OSM</a>',
@@ -30,7 +32,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Show sidebar
 sidebar.show();
-
+addBar();
 
 
 //取得JSON
@@ -40,6 +42,7 @@ xhr.send(null);
 xhr.onload = function () {
   maskData = JSON.parse(xhr.responseText);
   countrySelect();
+  getdata();
 }
 
 //篩選重複的市
@@ -108,7 +111,8 @@ function getValue(e) {
 }
 
 //取得data
-function getdata(countryUser) {
+// function getdata(countryUser) {
+function getdata() {
   let dataFilter = [];
   // let dataFilter = [{
   //   //geometry 經緯度
@@ -131,28 +135,28 @@ function getdata(countryUser) {
   for (let i = 0; i < maskData.features.length; i++) {
     //
     let zone = maskData.features[i].properties.address.substring(0, 3);
-    if (countryUser === zone) {
-      dataFilter.push({
-        geometry: maskData.features[i].geometry.coordinates.reverse(),
-        address: maskData.features[i].properties.address,
-        name: maskData.features[i].properties.name,
-        tel: maskData.features[i].properties.phone,
-        mask_adult: maskData.features[i].properties.mask_adult,
-        mask_child: maskData.features[i].properties.mask_child,
-        updated: maskData.features[i].properties.updated,
-        available: maskData.features[i].properties.available
-      })
-    }
+    // if (countryUser === zone) {
+    dataFilter.push({
+      geometry: maskData.features[i].geometry.coordinates.reverse(),
+      address: maskData.features[i].properties.address,
+      name: maskData.features[i].properties.name,
+      tel: maskData.features[i].properties.phone,
+      mask_adult: maskData.features[i].properties.mask_adult,
+      mask_child: maskData.features[i].properties.mask_child,
+      updated: maskData.features[i].properties.updated,
+      available: maskData.features[i].properties.available
+    })
+    // }
   }
   setMarker(dataFilter)
 }
 
+var markers = new L.MarkerClusterGroup().addTo(map);
 function setMarker(dataFilter) {
-  let marker = [];
-  let locatCenter = [];
-  let locatCenterReQ = [];
+  // let locatCenter = [];
+  // let locatCenterReQ = [];
   for (let i = 0; i < dataFilter.length; i++) {
-    let marker = L.marker(dataFilter[i].geometry)//.addTo(map)
+    markers.addLayer(L.marker(dataFilter[i].geometry)//.addTo(map)
       //彈出視窗
       .bindPopup(`
     <div class="l-popupWrap">
@@ -165,23 +169,39 @@ function setMarker(dataFilter) {
       <input type="button" value="成人口罩 ${dataFilter[i].mask_adult} 個" class="">
       <input type="button" value="兒童口罩 ${dataFilter[i].mask_child} 個" class="">
     </div>
-    `);
-    markerLayer = L.layerGroup([marker]);
+    `));
 
-    markerLayer.addTo(map);
+
     // 取得location中間值
-    locatCenterReQ.push({
-      x: dataFilter[i].geometry[0],
-      y: dataFilter[i].geometry[1]
-    });
+    // locatCenterReQ.push({
+    //   x: dataFilter[i].geometry[0],
+    //   y: dataFilter[i].geometry[1]
+    // });
   }
-  let resortX = locatCenterReQ.sort(function (a, b) { return b.x - a.x; });
-  let resortY = locatCenterReQ.sort(function (a, b) { return b.y - a.y; });
-  locatCenter = [resortX[Math.floor(resortX.length / 2)].x, resortY[Math.floor(resortY.length / 2)].y];
-  map.setView(locatCenter, 14);
-  console.log(locatCenter)
+  // let resortX = locatCenterReQ.sort(function (a, b) { return b.x - a.x; });
+  // let resortY = locatCenterReQ.sort(function (a, b) { return b.y - a.y; });
+  // locatCenter = [resortX[Math.floor(resortX.length / 2)].x, resortY[Math.floor(resortY.length / 2)].y];
+  // map.setView(locatCenter, 14);
+  // map.addLayer(markers);
 }
 
 function getRecentlocat() {
   console.log(map.locate());
+}
+
+//add sidebarsettingBtn
+function addBar() {
+  let sideBarBtn = document.querySelector('.leaflet-control-zoom');
+  sideBarBtn.innerHTML = `
+  <a class="leaflet-control-zoom-in" href="#" title="Zoom in" role="button" aria-label="Zoom in">+</a>
+  <a class="leaflet-control-zoom-out" href="#" title="Zoom out" role="button" aria-label="Zoom out">−</a>
+  <a class="showSideBtn" href="#" role="button"><i class="fas fa-bars"></i></a>
+  `
+  let showSideBtn = document.querySelector('.showSideBtn');
+  showSideBtn.addEventListener('click', toggleSideBar);
+}
+function toggleSideBar() {
+  sidebar.toggle();
+  let showSideBtn = document.querySelector('.showSideBtn');
+  showSideBtn.classList.toggle("sideBtn--ani");
 }
